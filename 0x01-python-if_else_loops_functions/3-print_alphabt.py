04dfd80b1f877f07ba076d5c4272ed3e7960ed96
Tony Mputhia <tonymmputhia@gmail.com>
2023-10-03 18:15:20 -0700

    Print with string format

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for value in range(ord('a'), ord('z') + 1):
     if value != ord('q') and value != ord('e'):
-        print(f"{chr(value)}", end='')
+        print("{}".format(chr(value)), end='')
