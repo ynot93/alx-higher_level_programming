82c29b011f55d15b08c900e6fba75facf380f969
Tony Mputhia <tonymmputhia@gmail.com>
2023-10-03 17:56:20 -0700

    Use only 2 print statements

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,8 +3,6 @@
 def uppercase(str):
     for ch in str:
         if ord(ch) >= ord('a') and ord(ch) <= ord('z'):
-            uppercase_ch = chr(ord(ch) - ord('a') + ord('A'))
-            print("{}".format(uppercase_ch), end='')
-        else:
-            print("{}".format(ch), end='')
-    print()
+            ch = chr(ord(ch) - ord('a') + ord('A'))
+        print("{}".format(ch), end='')
+    print("")
