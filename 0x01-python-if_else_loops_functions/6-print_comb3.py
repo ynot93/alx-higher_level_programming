6aabe704ec828ab3ab27bcf0954fa446331ea01f
Tony Mputhia <tonymmputhia@gmail.com>
2023-10-03 18:02:20 -0700

    Use required printing format

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for first in range(10):
     for second in range(first + 1, 10):
         if first == 8 and second == 9:
-            print(f"{first}{second}")
+            print("{}{}".format(first, second))
         else:
-            print(f"{first}{second}", end=", ")
+            print("{}{}".format(first, second), end=", ")
