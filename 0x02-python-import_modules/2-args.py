6437bf4be5aa1b397129271b3d03008160c25181
Tony Mputhia <tonymmputhia@gmail.com>
2023-10-05 03:40:10 -0700

    Use command line arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    
+    num = len(sys.argv) - 1
+    
+    if num == 1:
+        print("{} argument:".format(num))
+    elif num == 0:
+        print("{} arguments.".format(num))
+    else:
+        print("{} arguments:".format(num))
+
+    if num > 0:
+        for count in range(1, num + 1):
+            print("{}: {}".format(count, sys.argv[count]))
