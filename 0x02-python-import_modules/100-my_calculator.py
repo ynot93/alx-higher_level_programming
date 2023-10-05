98b193f530088c415d3c057f13c5d2f3accb4280
Tony Mputhia <tonymmputhia@gmail.com>
2023-10-05 11:13:49 -0700

    Fix error message

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py a operator b")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
     a = int(sys.argv[1])
