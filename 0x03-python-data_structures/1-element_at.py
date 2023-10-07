cda6e34888c6f1274ddb5960ea02f3ed90667a72
Tony Mputhia <tonymmputhia@gmail.com>
2023-10-07 07:53:23 -0700

    Fix out of range index

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,6 +3,6 @@ def element_at(my_list, idx):
     """Retrieve an element from a list."""
     if idx < 0:
         return None
-    if idx > len(my_list):
+    if idx >= len(my_list):
         return None
     return my_list[idx]
