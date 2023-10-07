5848514d7cbe60197211ea8eedff87597f0e2ffd
Tony Mputhia <tonymmputhia@gmail.com>
2023-10-07 07:38:39 -0700

    Retrieve element from list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    """Retrieve an element from a list."""
+    if idx < 0:
+        return "None"
+    if idx > len(my_list):
+        return "None"
+    return my_list[index]
