c145502a3948ae63425a0ae6250912775a508087
Tony Mputhia <tonymmputhia@gmail.com>
2023-10-08 23:47:39 -0700

    Fix conditional error

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     """Returns tuple with string length and first character."""
-    length = len(sentence)
-
     if sentence == "":
-        first = None
+        return (0, None)
 
+    length = len(sentence)
     first = sentence[0]
 
     return (length, first)
