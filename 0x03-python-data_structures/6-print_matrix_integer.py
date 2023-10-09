8651f9d025ba8d06a15e4da17754012ebb8ca95f
Tony Mputhia <tonymmputhia@gmail.com>
2023-10-08 22:38:22 -0700

    Print matrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    """Print a matrix of integers"""
+    if matrix is None:
+        return None
+    
+    for item in matrix:
+        row = ""
+        for num in item:
+            row += "{:d} ".format(num)
+        
+        print(row.strip())
