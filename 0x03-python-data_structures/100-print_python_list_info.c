4b43552e399e8b41e692f008f03a76d4c2166012
Tony Mputhia <tonymmputhia@gmail.com>
2023-10-09 03:56:08 -0700

    CPython

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,20 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t size, alloc, i;
+	PyObject *item;
+
+	size = PyList_Size(p);
+	alloc = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %zd\n", size);
+	printf("[*] Allocated = %zd\n", alloc);
+
+	for (i = 0; i < size; i++)
+	{
+		item = PyList_GetItem(p, i);
+		printf("Element %zd: %s\n", i, Py_TYPE(item)->tp_name);
+	}
+}
