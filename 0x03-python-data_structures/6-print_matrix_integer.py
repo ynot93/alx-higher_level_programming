57ba60e2a525c5e80b60885fa1c8c9feb718f455
Tony Mputhia <tonymmputhia@gmail.com>
2023-10-10 05:45:36 -0700

    Fix pycodestyle error

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,10 +3,10 @@ def print_matrix_integer(matrix=[[]]):
     """Print a matrix of integers"""
     if matrix is None:
         return None
-    
+
     for item in matrix:
         row = ""
         for num in item:
             row += "{:d} ".format(num)
-        
-        print(row.strip())
+
+    print(row.strip())
