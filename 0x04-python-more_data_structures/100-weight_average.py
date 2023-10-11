39ab6e47f2248c27e98968c1818b0d72bebff76f
Tony Mputhia <tonymmputhia@gmail.com>
2023-10-11 15:18:56 -0700

    Find weighted average of integers

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    """Return weighted average of all integers"""
+    if not my_list:
+        return 0
+
+    total_weight = 0
+    total_score = 0
+
+    for score, weight in my_list:
+        total_score += score * weight
+        total_weight += weight
+
+    if total_weight == 0:
+        return 0
+
+    avg = total_score / total_weight
+
+    return avg
