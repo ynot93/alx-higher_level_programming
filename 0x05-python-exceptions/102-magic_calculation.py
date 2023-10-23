1fbdf329542f65c40f05d43046569d7befb35a7f
Tony Mputhia <tonymmputhia@gmail.com>
2023-10-23 15:26:39 -0700

    Python Bytecode with exception

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+
+    for i in range(1, 4):
+        try:
+            if i > a:
+                raise Exception("Too far")
+        except Exception as e:
+            return e
+        result += (a ** b) / i
+    return result
