799322a1902468ee6a74885bd9f0c36c47b3580c
Tony Mputhia <tonymmputhia@gmail.com>
2023-10-23 15:39:39 -0700

    Replace double quotes with single quotes

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -5,7 +5,7 @@ def magic_calculation(a, b):
     for i in range(1, 4):
         try:
             if i > a:
-                raise Exception("Too far")
+                raise Exception('Too far')
             result += (a ** b) / i
         except Exception:
             result = b + a
