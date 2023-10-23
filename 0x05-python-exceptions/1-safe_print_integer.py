9af751e7d379b7a12a08e64f6310d9ccb345fdbf
Tony Mputhia <tonymmputhia@gmail.com>
2023-10-23 11:36:29 -0700

    Safe printing of integers

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """Print an integer with string format"""
+    try:
+        print("{:d}".format(int(value)))
+        return True
+    except TypeError:
+        return False
+    except ValueError:
+        return False
