4270a7da26d826338590dcb8194f6333017bd3c4
Tony Mputhia <tonymmputhia@gmail.com>
2023-10-23 14:36:15 -0700

    Print int with error message in stderr

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    """Prints integer with error message"""
+    try:
+        print("{:d}".format((value)))
+        return True
+    except TypeError as te:
+        print(f"Exception: {te}", file=sys.stderr)
+        return False
+    except ValueError as ve:
+        print(f"Exception: {ve}", file=sys.stderr)
+        return False
