db3ae77cbb0cbf725fa112d832103d7eded2b314
Tony Mputhia <tonymmputhia@gmail.com>
2023-10-23 11:32:00 -0700

    Fix logical errors

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,12 +5,9 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     try:
         for i in range(x):
-            print ("{}".format(my_list[i]), end = " ")
+            print("{}".format(my_list[i]), end="")
             count += 1
     except IndexError:
         pass
-    except TypeError:
-        pass
     print()
-    
     return count
