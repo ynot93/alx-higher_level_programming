52dbeb838db973cbd8ddd90f6ebd2f0dca7594f2
Tony Mputhia <tonymmputhia@gmail.com>
2023-10-23 14:39:03 -0700

    Fix pycodestyle error

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     """Print an integer with string format"""
     try:
-        print("{:d}".format(int(value)))
+        print("{:d}".format(value))
         return True
     except (TypeError, ValueError):
         return False
