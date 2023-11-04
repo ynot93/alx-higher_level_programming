363c9204ad91198ba9fcc9fad8166c23b67f470f
Tony Mputhia <tonymmputhia@gmail.com>
2023-11-04 00:28:36 -0700

    Fix failing errors

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -9,27 +9,28 @@
  */
 void print_python_string(PyObject *p)
 {
-	long int l;
-
-	fflush(stdout);
+	Py_ssize_t length;
+	const wchar_t *value = PyUnicode_AsUnicode(p);
 
 	printf("[.] string object info\n");
 
-	if (strcmp(p->ob_type->tp_name, "str") != 0)
+	if (PyUnicode_Check(p))
 	{
-		printf("  [ERROR] Invalid String Object\n");
-		return;
-	}
-	l = ((PyASCIIObject *)(p))->length;
+		if (PyUnicode_IS_COMPACT_ASCII(p))
+		{
+			printf("  type: compact ascii\n");
+		}
+		else
+		{
+			printf("  type: compact unicode object\n");
+		}
 
-	if (PyUnicode_IS_COMPACT_ASCII(p))
-	{
-		printf("  type: compact unicode object\n");
+		length = PyUnicode_GET_LENGTH(p);
+		printf("  length: %ld\n", length);
+		printf("  value: %ls\n", value);
 	}
 	else
 	{
-		printf("  type: compact ascii\n");
+		printf("  [ERROR] Invalid String Object\n");
 	}
-	printf("  length: %ld\n", l);
-	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &l));
 }
