c23b7d1fe6d4489771aec2c08b0be01a5ef59c72
Tony Mputhia <tonymmputhia@gmail.com>
2023-11-07 03:01:51 -0800

    Fix failing tests

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,11 +4,12 @@ This module handles Python Input/Output operations.
 
 """
 
+
 def read_file(filename=""):
     """
     Reads a text file(UTF8) and prints it to stdout.
 
     """
-    with open(filename, 'r') as file:
+    with open(filename, 'r', encoding='utf-8') as file:
         data = file.read()
-        print(data)
+        print(data, end='')
