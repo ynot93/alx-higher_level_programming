23868a46fd2dcb314f576df1c10adf79c39c5998
Tony Mputhia <tonymmputhia@gmail.com>
2023-11-07 13:46:46 -0800

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This module deals with JSON (de)serialization.
+
+"""
+
+
+class Student:
+    """
+    Defines a student.
+
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize student with name and age attributes.
+
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns dict representation of a Student instance.
+
+        """
+        if attrs is None or not isinstance(attrs, list):
+            return self.__dict__
+        else:
+            filtered_dict = {}
+            for item in attrs:
+                if hasattr(self, item):
+                    filtered_dict[item] = getattr(self, item)
+            return filtered_dict
