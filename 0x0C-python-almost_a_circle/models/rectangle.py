a8cce7432f5d5157ccf7352e4fae4488bd9bc5b6
Tony Mputhia <tonymmputhia@gmail.com>
2023-11-10 05:28:18 -0800

    Display #1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -117,8 +117,10 @@ class Rectangle(Base):
         Prints rectangle instance with # character.
 
         """
+        for _ in range(self.y):
+            print()
         for _ in range(self.height):
-            print("#" * self.width)
+            print(" " * self.x + "#" * self.width)
 
     def __str__(self):
         """
