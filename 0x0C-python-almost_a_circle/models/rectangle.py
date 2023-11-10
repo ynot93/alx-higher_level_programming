0159e6c28ff1632367b8017200458e22b3251ea3
Tony Mputhia <tonymmputhia@gmail.com>
2023-11-10 04:28:32 -0800

    Validate attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -42,7 +42,7 @@ class Rectangle(Base):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
-            raise ValueError("value must be > 0")
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -62,7 +62,7 @@ class Rectangle(Base):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
-            raise ValueError("value must be > 0")
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -81,6 +81,8 @@ class Rectangle(Base):
         """
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -99,4 +101,6 @@ class Rectangle(Base):
         """
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
