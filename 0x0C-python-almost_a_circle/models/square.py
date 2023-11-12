055f1a17d9ad23c76374ff10dbbefb47e6ad650c
Tony Mputhia <tonymmputhia@gmail.com>
2023-11-12 05:13:15 -0800

    Define update module and return dict rep of square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,6 +18,51 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """
+        Getter function for size of square.
+
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """
+        Seter function for size of square.
+
+        """
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """
+        Assigns attributes to variable (key-word) arguments.
+        
+        """
+        argmnts = ['id', 'size', 'x', 'y']
+
+        if args:
+            for i, arg in enumerate(args):
+                setattr(self, argmnts[i], arg)
+
+        else:
+            for key, value in kwargs.items():
+                if key in argmnts:
+                    setattr(self, key, value)
+
+    def to_dictionary(self):
+        """
+        Returns dictionary representation of Square.
+
+        """
+        return {
+                "id": self.id,
+                "size": self.size,
+                "x": self.x,
+                "y": self.y
+        }
+
     def __str__(self):
         """
         Returns information about a square object.
