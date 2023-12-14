fc149020ad3b4f701aef68f010111876da0b2297
Tony Mputhia <tonymmputhia@gmail.com>
2023-12-14 11:01:53 -0800

    Genre ID for all shows

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,8 @@
+-- List all shows displaying tv_shows.title - tv_show_genres.genre_id
+-- Sort results in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- Display NULL if show doesn't have a genre
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, COALESCE(tv_show_genres.genre_id, 'NULL') ASC;
