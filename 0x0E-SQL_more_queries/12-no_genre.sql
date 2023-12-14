52d810d5b4df376643fd2cfcf00ffab26c0144d4
Tony Mputhia <tonymmputhia@gmail.com>
2023-12-14 11:20:40 -0800

    No genre

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- List all shows without a genre linked
+-- Display tv_shows.title - tv_show_genres.genre_id in ascending order
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL OR tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
