e81ff201c81ee557fad5495a4f66bee2f2c7efba
Tony Mputhia <tonymmputhia@gmail.com>
2023-12-15 11:20:21 -0800

    Always a name

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,9 +1,6 @@
 -- Create the table force_name with id and name
 
-USE hbtn_0d_2;
-
 CREATE TABLE IF NOT EXISTS force_name (
 	id INT,
-	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY (id)
+	name VARCHAR(256) NOT NULL
 );
