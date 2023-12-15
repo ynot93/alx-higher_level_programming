b2f413590d8f1901472f57213a7580e72f4a442b
Tony Mputhia <tonymmputhia@gmail.com>
2023-12-15 10:59:38 -0800

    Fix display NULL if no genre

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -2,7 +2,7 @@
 -- Sort results in ascending order by tv_shows.title and tv_show_genres.genre_id
 -- Display NULL if show doesn't have a genre
 
-SELECT tv_shows.title, tv_show_genres.genre_id
+SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, 'NULL') as genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC, COALESCE(tv_show_genres.genre_id, 'NULL') ASC;
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
