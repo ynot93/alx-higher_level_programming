bdafb8b3845675dcb46571dbe31754f931965574
Tony Mputhia <tonymmputhia@gmail.com>
2023-12-15 11:04:56 -0800

    Cities of California correction

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -3,5 +3,5 @@
 
 SET @california_id = (SELECT id FROM states WHERE name = 'California');
 
-SELECT * FROM cities WHERE state_id = @california_id
+SELECT id, name FROM cities WHERE state_id = @california_id
 ORDER BY id ASC;
