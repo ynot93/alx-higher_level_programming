6b1417e31a0a169aebdaa02dd6411f8745751a4d
Tony Mputhia <tonymmputhia@gmail.com>
2024-01-10 07:53:54 +0000

    Sorted occurences

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const dict = require('./101-data').dict;
+
+newDict = {};
+
+for (const userId in dict) {
+  const occurences = dict[userId];
+
+  if (!newDict[occurences]) {
+    newDict[occurences] = [];
+  }
+  newDict[occurences].push(userId);
+}
+console.log(newDict);
