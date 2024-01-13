82b7f7e4362442a9d20e20778b52b7a3497bfcb5
Tony Mputhia <tonymmputhia@gmail.com>
2024-01-13 08:52:25 -0800

    Prevent SQL injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Display all values in states where name matches the argument
+
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    username, password, database, state_name = sys.argv[1:]
+
+    db_config = {
+            'host': 'localhost',
+            'port': 3306,
+            'user': username,
+            'passwd': password,
+            'db': database
+    }
+
+    db = MySQLdb.connect(**db_config)
+
+    cursor = db.cursor()
+
+    query = "SELECT * FROM states WHERE name = %s \
+            ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
