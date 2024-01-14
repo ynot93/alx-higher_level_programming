c0c35fd5e53625580c815eb2da5d9ad071f2f7b6
Tony Mputhia <tonymmputhia@gmail.com>
2024-01-14 06:49:13 -0800

    Delete statse with a

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Delete all State objects starting with 'a' from hbtn_0e_6_usa
+using SQLAlchemy,
+
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    username, password, database = sys.argv[1:4]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(username, password, database),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    states = session.query(State).filter(State.name.like('%a%')).all()
+
+    for state in states:
+        session.delete(state)
+
+    session.commit()
+    session.close()
