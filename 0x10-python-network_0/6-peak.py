8c3bb909def04c2f6694713e4461937df48db7d4
Tony Mputhia <tonymmputhia@gmail.com>
2024-01-25 11:08:56 -0800

    Find peak in list of assorted numbers

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Determines the peak in a list of numbers """
+def find_peak(list_of_integers):
+    """
+    Uses binary search to find peak.
+
+    """
+    if not list_of_integers:
+        return None
+
+    low, high = 0, len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+        mid_num = list_of_integers[mid]
+
+        if list_of_integers[mid] >= list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
+
+    return list_of_integers[low]
