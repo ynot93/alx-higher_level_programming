769a0262ffe59a1211326e0b5e39a38dcac67cf8
Tony Mputhia <tonymmputhia@gmail.com>
2024-01-25 01:23:08 -0800

    Modify script to meet requirements

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-# Sends request to URL and displays the size of the body
-# of the response
-
-if [ -z "$1" ]; then
-  echo "Usage: "$0" <url>"
-  exit 1
-fi
-
+# Sends request to URL and displays the size of the body of the response
 curl -sI "$1" | grep Content-Length | awk '{print $2}'
