9e46549bb75c9f0b4836183a6f05aaee4333fe9f
Tony Mputhia <tonymmputhia@gmail.com>
2024-01-26 05:25:47 -0800

    Send email with POST request

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Sends Email as POST request and display response body
+
+"""
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+
+    with urllib.request.urlopen(url, data) as response:
+        body = response.read().decode('utf-8')
+    print(body)
