445df4c3c2aba734451d5e48730f89d7a275d689
Tony Mputhia <tonymmputhia@gmail.com>
2024-01-26 05:42:12 -0800

    Use requests module

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Fetches data from the URL https://alx-intranet.hbtn.io/status using
+requests module.
+
+"""
+import requests
+
+if __name__ == '__main__':
+    url = "https://alx-intranet.hbtn.io/status"
+
+    response = requests.get(url)
+
+    print("Body response:")
+    print(f"\t- type: {type(response.text)}")
+    print(f"\t- content: {response.text}")
