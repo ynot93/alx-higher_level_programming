450fca81f147f03f80d4dfc1e2583b92ddaaeea2
Tony Mputhia <tonymmputhia@gmail.com>
2024-01-26 05:00:46 -0800

    Make script un-importable

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,12 +5,13 @@ Fetches data from the URL https://alx-intranet.hbtn.io/status
 """
 import urllib.request
 
-url = "https://alx-intranet.hbtn.io/status"
-with urllib.request.urlopen(url) as response:
-    content = response.read()
-    html = content.decode('utf-8')
+if __name__ == '__main__':
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        html = content.decode('utf-8')
 
-print("Body response:")
-print(f"\t- type : {type(content)}")
-print(f"\t- content : {content}")
-print(f"\t- utf8 content : {html}")
+    print("Body response:")
+    print(f"\t- type : {type(content)}")
+    print(f"\t- content : {content}")
+    print(f"\t- utf8 content : {html}")
