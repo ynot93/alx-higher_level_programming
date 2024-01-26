324e237bc5b7d28ff2b66716d2887bb77150cee6
Tony Mputhia <tonymmputhia@gmail.com>
2024-01-26 04:57:00 -0800

    Fetch url using with

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Fetches data from the URL https://alx-intranet.hbtn.io/status
+
+"""
+import urllib.request
+
+url = "https://alx-intranet.hbtn.io/status"
+with urllib.request.urlopen(url) as response:
+    content = response.read()
+    html = content.decode('utf-8')
+
+print("Body response:")
+print(f"\t- type : {type(content)}")
+print(f"\t- content : {content}")
+print(f"\t- utf8 content : {html}")
