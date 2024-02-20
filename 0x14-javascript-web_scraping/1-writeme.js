0a69a2bd38296b569ade29845180760e1d4238e2
Tony <tonymmputhia@gmail.com>
2024-02-20 13:13:10 -0800

    Write string to file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const fileName = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(fileName, content, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
