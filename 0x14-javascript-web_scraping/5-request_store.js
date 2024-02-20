dc17a49bf109dcb3d3aa1552b809d3463bf70f7c
Tony <tonymmputhia@gmail.com>
2024-02-20 14:30:24 -0800

    Store webpage content in a file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+
+const filePath = process.argv[3];
+
+request.get(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  fs.writeFile(filePath, body, 'utf-8', (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+});
