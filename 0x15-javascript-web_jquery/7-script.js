bfee68ed28ad554f671acc2785f17a07fb6d69ba
Tony Mputhia <tonymmputhia@gmail.com>
2024-02-29 05:18:39 +0300

    Fetch Star Wars character

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  const url = 'https://swapi-api.alx-tools.com/api/people/5/?format=json';
+  $.get(url, function (data) {
+    const characterName = data.name;
+
+    $('#character').text(characterName);
+  });
+});
