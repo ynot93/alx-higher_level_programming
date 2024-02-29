82d53d2331aab4519974ef4adb51859a14c4ae92
Tony Mputhia <tonymmputhia@gmail.com>
2024-02-29 05:46:39 +0300

    Fetches and prints hello depending on the language

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  function fetchTranslation() {
+    const languageCode = $("#language_code").val();
+
+    url = "https://www.fourtonfish.com/hellosalut/hello/"
+    $.get(url, { lang: languageCode }, function(data) {
+      const helloTranslation = data.hello;
+
+      $("#hello").text(helloTranslation);
+    });
+  }
+
+  $("#btn_translate").click(fetchTranslation);
+
+  $("#language_code").keypress(function(event) {
+    if (event.which === 13) {
+      fetchTranslation();
+    }
+  });
+});
+
