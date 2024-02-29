bb03f57184d2f2132b6b71773168b2115e0002ed
Tony Mputhia <tonymmputhia@gmail.com>
2024-02-29 05:30:31 +0300

    Fix semistandard error

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,12 +1,12 @@
-$(document).ready(function() {
-  url = "https://swapi-api.alx-tools.com/api/films/?format=json"
-  $.get(url, function(data) {
-    data.results.forEach(function(movie) {
+$(document).ready(function () {
+  const url = 'https://swapi-api.alx-tools.com/api/films/?format=json';
+  $.get(url, function (data) {
+    data.results.forEach(function (movie) {
       const title = movie.title;
-      
-      const listItem = $("<li>").text(title);
-      
-      $("#list_movies").append(listItem);
+
+      const listItem = $('<li>').text(title);
+
+      $('#list_movies').append(listItem);
     });
   });
 });
