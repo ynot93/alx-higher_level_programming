6808552d19c88f26d7d972da203f784264760e48
Tony Mputhia <tonymmputhia@gmail.com>
2024-02-29 05:28:32 +0300

    List Star Wars movie titles

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  url = "https://swapi-api.alx-tools.com/api/films/?format=json"
+  $.get(url, function(data) {
+    data.results.forEach(function(movie) {
+      const title = movie.title;
+      
+      const listItem = $("<li>").text(title);
+      
+      $("#list_movies").append(listItem);
+    });
+  });
+});
